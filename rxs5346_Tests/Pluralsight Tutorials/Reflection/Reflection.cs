75de05847791e31dcdcc74ae11e555cda74e34fd
rxs5346 <rxs5346@sru.edu>
2015-02-12 08:57:58 -0500

    added more training

---
+++ rxs5346_Tests/Pluralsight Tutorials/Reflection/Reflection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Reflection
+{
+    public class Animal
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+    class Reflection
+    {
+        static void Main(string[] args)
+        {
+            var type = typeof(Reflection);
+            var members = type.GetMembers(BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+            foreach (var memberInfo in members)
+            {
+                Console.WriteLine(memberInfo.Name);
+            }
+            Console.WriteLine("BREAK");
+
+            //this isnt working like the demo... should return required for name
+            //Type type2 = typeof(Animal);
+            PropertyInfo propertyInfo = type.GetProperty("Name");
+            object[] attributes = propertyInfo.GetCustomAttributes(false);
+            foreach (Attribute attribute in attributes)
+            {
+                Console.WriteLine(attribute.GetType().Name);
+            }
+        }
+    }
+}
---
+++ rxs5346_Tests/Pluralsight Tutorials/Threads/Threads.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    class Threads
+    {
+        static void Main(string[] args)
+        {
+            DownloadAsynchronously();
+            Console.WriteLine("Waiting to finish on thread {0}...",Thread.CurrentThread.ManagedThreadId);
+        }
+        private static void DownloadAsynchronously()
+        {
+            string[] urls = { "http://www.microsoft.com/en-us/default.aspx",
+                              "http://twitter.com/odetocode",
+                            "http://www.pluralsight.com/courses/csharp-fundamentals"};
+          //  foreach (var url in urls) //not the easiest way to use threads 
+            //{
+              //  var thread = new Thread(Download);
+                //thread.Start(url);
+            //}
+            //foreach (var url in urls)
+            //{
+              //  Download(url);
+            //}
+            //best way to do it
+            Parallel.ForEach(urls, url =>
+                                       {
+                                           var client = new WebClient();
+                                           var html = client.DownloadString(url.ToString());
+                                           Console.WriteLine("Download {0} chars from {1} on thread {2}", html.Length, url, Thread.CurrentThread.ManagedThreadId);
+                                           //Console.WriteLine(html.ToString());
+                                       });
+        }
+        private static void Download(String url) //needs to be object to do multithreading... 
+        {
+           // var client = new WebClient();
+            //var html = client.DownloadString(url.ToString());
+            //Console.WriteLine("Download {0} chars from {1} on thread {2}", html.Length, url, Thread.CurrentThread.ManagedThreadId);
+
+            var client = new WebClient();
+            client.DownloadDataCompleted += client_DownloadDataCompleted;
+            client.DownloadStringAsync(new Uri(url),url);
+            
+        }
+
+        static void client_DownloadDataCompleted(object sender, DownloadDataCompletedEventArgs e)
+        {
+            var html = e.Result;
+            var url = e.UserState as string;
+            Console.WriteLine("Download {0} chars from {1} on thread {2}", html.Length, url, Thread.CurrentThread.ManagedThreadId);
+        }
+
+    }
+}
